573a5d6b1728b77b081047dcb7febc2202d90b21
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-29 12:11:47 +0530

    header done content need to work on

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './css/ProductItem.css'
 
 const ProductItem = ({ data }) => {
     return (
-        <div style={{ display: "flex", gap: "20px", border: "1px solid black" }}>
+        <div style={{ display: "flex", gap: "20px", border: "1px solid black", width: "100%" }}>
+            <hr/>
             <div className='item-header'>
-                    <p>
-                        <b>From</b><br />
-                        {data.moving_from}
-                    </p>
+                <p>
+                    <b>From</b><br />
+                    {data.moving_from}
+                </p>
 
-                    <i class="bi bi-arrow-right-circle" style={{color:"#9c0101",fontSize: "25px"}}></i>
+                <p className='icons'>
+                    <i class="bi bi-arrow-right-circle"
+                        style={{
+                            color: "#9c0101",
+                            fontSize: "25px",
+                        }}></i>
+                </p>
 
-                    <p>
-                        <b>To</b><br />
-                        {data.moving_to}
-                    </p>
+                <p>
+                    <b>To</b><br />
+                    {data.moving_to}
+                </p>
 
-                    <p>
-                        <b>Request#</b><br />
-                        <b style={{color:"#9c0101"}}>{data.estimate_id}</b>
-                    </p>
+                <p>
+                    <b>Request#</b><br />
+                    <b style={{ color: "#9c0101" }}>{data.estimate_id}</b>
+                </p>
             </div>
             <div className='item-content'></div>
             <div className='item-footer'></div>
