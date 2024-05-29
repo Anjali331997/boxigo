427013cb9280bbe69c23a526a26cb3fab86818d8
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-29 17:35:49 +0530

    adding the css

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ItemInventoryCollapse from './ItemInventoryCollapse';
-
+import './css/ProductDetails.css'
 
 
 const ProductDetails = ({ details }) => {
@@ -8,78 +8,73 @@ const ProductDetails = ({ details }) => {
 
   return (
     <div className='productDetails'>
-      <hr />
-      <div>
-        <h4>Additional Information</h4>
-        <button>Edit Additional Info</button>
+      <div className='edit-div'>
+        <h4 className='edit-headings'>Additional Information</h4>
+        <button className='edit-btn'>Edit Additional Info</button>
       </div>
 
-      <p>
+      <p className='test-data-para'>
         Test Data
       </p>
 
-      <div>
-        <h4>House Details</h4>
-        <button>Edit House Details</button>
+      <div className='edit-div'>
+        <h4 className='edit-headings'>House Details</h4>
+        <button className='edit-btn'>Edit House Details</button>
       </div>
 
-      <div>
-        <h4>Existion House Details</h4>
+      <div className='houseDetails-div'>
+        <h4 className='houseDetails-heading'>Existing House Details</h4>
         <div>
-          <p> <b>Floor No.</b>
+          <p>
+            <b>Floor No.</b>
             <br />
             {details.old_floor_no}
           </p>
-        </div>
-
-        <div>
-          <p> <b>Elevator Available.</b>
+          <p>
+            <b>Elevator Available.</b>
             <br />
             {details.old_elevator_availability}
           </p>
-        </div>
-
-        <div>
-          <p> <b>Distance from Elevator/Staircase to truck.</b>
+          <p>
+            <b>Distance from Elevator/Staircase to truck.</b>
             <br />
             {details.old_parking_distance}
           </p>
         </div>
+
+
       </div>
 
-      <div>
-        <h4>New House Details</h4>
+      <div className='houseDetails-div'>
+        <h4 className='houseDetails-heading'>New House Details</h4>
         <div>
-          <p> <b>Floor No.</b>
+          <p>
+            <b>Floor No.</b>
             <br />
             {details.new_floor_no}
           </p>
-        </div>
-
-        <div>
-          <p> <b>Elevator Available.</b>
+          <p>
+            <b>Elevator Available.</b>
             <br />
             {details.new_elevator_availability}
           </p>
-        </div>
-
-        <div>
-          <p> <b>Distance from Elevator/Staircase to truck.</b>
+          <p>
+            <b>Distance from Elevator/Staircase to truck.</b>
             <br />
             {details.new_parking_distance}
           </p>
         </div>
       </div>
 
-      <div>
-        <h4>Inventory Details</h4>
-        <button>Edit Inventory</button>
+      <div className='edit-div'>
+        <h4 className='edit-headings'>Inventory Details</h4>
+        <button className='edit-btn'>Edit Inventory</button>
       </div>
 
       <div>
         {
           inventory.map((ele, index) => {
-           return <ItemInventoryCollapse key={index} data={ele}/>
+            return <ItemInventoryCollapse key={index} data={ele} />
           })
         }
       </div>
