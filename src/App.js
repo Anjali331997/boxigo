dd8e466b75c624cdbf378059cbb4cb52355a052a
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-29 08:29:44 +0530

    adding the bootstrap icons to sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Sidebar/>
+    </BrowserRouter>
   );
 }
 
