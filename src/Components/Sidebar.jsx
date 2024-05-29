581c11bfbc4abf78cf8d6ee4cff803c4a8d2af4f
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-29 18:46:12 +0530

    adding the whole code to git hub working on css

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -11,7 +11,7 @@ import {
 
 const Sidebar = () => {
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
+    <div style={{ display: 'flex', overflow: 'scroll initial' }}>
     <CDBSidebar textColor="#fff" backgroundColor="#9c0101">
       <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
         <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
