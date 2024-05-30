126ba0ce68b547db5a3c23d4b6175e2c61a923e0
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-30 12:16:20 +0530

    production item skeleton

--- src/Components/ProductionItemSkeleton.jsx
+++ src/Components/ProductionItemSkeleton.jsx
@@ -7,7 +7,7 @@ const ProductionItemSkeleton = () => {
     <div className='item-container' >
             <hr />
             <div className='item-header'>
-                <p className='placeholder-glow'>
+                <p className='placeholder'>
                     <b>From</b><br />
                 </p>
 
@@ -19,7 +19,7 @@ const ProductionItemSkeleton = () => {
                         }}></i>
                 </p>
 
-                <p className='placeholder-glow'>
+                <p className='placeholder'>
                     <b>To</b><br />
                 </p>
 
@@ -31,21 +31,21 @@ const ProductionItemSkeleton = () => {
 
             <div className='item-content'>
                 <div className='infoDiv'>
-                    <p className='placeholder-glow'>
+                    <p className='placeholder'>
                         <i className="bi bi-house-door-fill"></i>
                         house
                     </p>
-                    <p className='placeholder-glow'>
+                    <p className='placeholder'>
                         <i className="bi bi-luggage-fill"></i>
                         no of package
                      
                     </p>
-                    <p className='placeholder-glow'>
+                    <p className='placeholder'>
                         <i className="bi bi-geo-alt-fill"></i>
                         distance
                        
                     </p>
-                    <p className='placeholder-glow'>
+                    <p className='placeholder'>
                         <i className="bi bi-calendar3"></i>
                         date-date-date
                         
@@ -66,7 +66,7 @@ const ProductionItemSkeleton = () => {
 
             <div className='item-footer placeholder'>
                 <i class="bi bi-exclamation-triangle-fill" className='placeholder'></i>
-                <p className='placeholder-glow'><b>Disclaimer:</b>Please update your move date before two days of shifting</p>
+                <p className='placeholder'><b>Disclaimer:</b>Please update your move date before two days of shifting</p>
             </div>
         </div>
   )
